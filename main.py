14df61cb261134967d74a653cb5637f19bf29eb2
SzP <szpimportant@gmail.com>
2025-11-02 09:31:39 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Main entry point for the application.
+"""
+
+def main():
+    """Main function."""
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+Main package for the application.
+"""
+
+__version__ = "0.1.0"
+
